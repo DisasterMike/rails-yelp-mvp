528bf95c2ce1c33f46fa4624925982f21e19d2f8
michael stump <maikerustump@gmail.com>
2024-07-03 00:34:50 +0900

    home and show finished

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,62 @@
+class RestaurantsController < ApplicationController
+  before_action :get_restaurant, only: [:show]
+
+  def index
+    # raise
+    @restaurants = Restaurant.all
+    @pics = []
+    @restaurants.each do |restaurant|
+      @pics << get_restaurant_pic(restaurant.category)
+    end
+  end
+
+  def show
+  end
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
+  private
+
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    @pic = get_restaurant_pic(@restaurant.category)
+  end
+
+  FOOD_IMG = {
+    japanese: "Japanese.jpg",
+    belgian: "belgian.jpg",
+    french:  "french.jpeg",
+    chinese:  "chinese.jpg",
+    italian:  "italian.jpg"
+  }
+
+  def get_restaurant_pic(category)
+    case category
+    when "japanese"
+      FOOD_IMG[:japanese]
+    when "belgian"
+      FOOD_IMG[:belgian]
+    when "chinese"
+      FOOD_IMG[:chinese]
+    when "french"
+      FOOD_IMG[:french]
+    when "italian"
+      FOOD_IMG[:italian]
+    else
+      nil
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# FOOD_IMG = {
+#   japanese: "images/Japanese.jpg",
+#   belgian: "images/belgian.jpg",
+#   french:  "images/french.jpeg",
+#   chinese:  "images/chinese.jpg",
+#   italian:  "images/italian.jpg"
+# }
+
+puts "Generating 5 restaurants..."
+
+5.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Name.name
+  restaurant.address = Faker::Address.full_address
+  restaurant.phone_number = Faker::Number.number(digits: 11)
+  restaurant.category = %w[chinese italian japanese french belgian].sample
+  if restaurant.save
+    puts "Added a restaurant called #{restaurant.name} to the database"
+  else
+    puts "Couldn't add #{restaurant.name} to the database"
+  end
+end
+
+puts "Finished adding restaurants!"
